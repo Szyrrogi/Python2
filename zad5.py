0b1695c3df6c315fd7161486e7baf99a5a629980
Igor Różanowski <62307954+Szyrrogi@users.noreply.github.com>
2025-03-04 16:41:33 +0100

    Update zad5.py

--- zad5.py
+++ zad5.py
@@ -5,7 +5,6 @@ def transposition_cipher(text: str, key: int) -> str:
     text_list = list(text)
 
     for i in range(0, len(text_list) - key + 1, key):
-        # Zamiana i-tej litery z (i + key - 1)-tą literą
         text_list[i], text_list[i + key - 1] = text_list[i + key - 1], text_list[i]
 
     return ''.join(text_list)
